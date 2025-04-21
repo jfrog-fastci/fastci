1a458aada1d50ffa51700445dbf822266ea05da0
Shay Dahan <shayd@jfrog.com>
2025-04-21 17:36:30 +0300

    Update exposeRuntime function to log and export only GITHUB_ environment variables

--- src/export-gh-env.ts
+++ src/export-gh-env.ts
@@ -1,8 +1,9 @@
 import * as core from '@actions/core';
 
 export async function exposeRuntime() {
+  core.info('Exposing runtime environment variables starting with GITHUB_');
   Object.keys(process.env).forEach(function (key) {
-    if (key.startsWith('ACTIONS_')) {
+    if (key.startsWith('GITHUB_')) {
       core.info(`${key}=${process.env[key]}`);
       core.exportVariable(key, process.env[key]);
     }
