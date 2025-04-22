cfc1f587c49e7107b4269364053c2ff1840fe146
Shay Dahan <shayd@jfrog.com>
2025-04-22 15:15:20 +0300

    change log level

--- src/otel-cicd-action/trace/job.ts
+++ src/otel-cicd-action/trace/job.ts
@@ -14,13 +14,12 @@ import { ProcessTree } from "../../types/process";
 import { debug } from "@actions/core";
 
 async function traceJob(processTrees: ProcessTree[], job: components["schemas"]["job"], annotations?: components["schemas"]["check-annotation"][]) {
+  debug(`Tracing job ${job.name}`);
   const tracer = trace.getTracer("otel-cicd-action");
 
   if (!job.completed_at) {
     job.completed_at = new Date().toISOString();
   }
-
-  debug(JSON.stringify(job, null, 2));
 
   const startTime = new Date(job.started_at);
   const completedTime = new Date(job.completed_at);
