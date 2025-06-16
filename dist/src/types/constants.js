a83bc036d1d40eafa6aaeef4acaea781595536e0
Shay Dahan <shayd@jfrog.com>
2025-06-16 15:01:51 +0300

    remove old dist

--- dist/src/types/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TRIGGER_FILE_PATH = exports.PROCESS_TREES_PATH = exports.FASTCI_TEMP_DIR = void 0;
-exports.FASTCI_TEMP_DIR = '/tmp/fastci';
-exports.PROCESS_TREES_PATH = `${exports.FASTCI_TEMP_DIR}/process_trees.json`;
-exports.TRIGGER_FILE_PATH = `${exports.FASTCI_TEMP_DIR}/trigger`;
